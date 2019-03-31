ed66abdc5340345933cb36317204affad783b576
thelordofscrubs <ekaituri@gmail.com>
2019-03-31 12:27:04 +0300

    worked in visualizing ascii array and cutting picture into 8x17 bits

--- main.py
+++ main.py
@@ -5,28 +5,6 @@ import numpy as np
 
 print("please input the file name\n(This program was written to work with PNG and JPEG, but may work with others)")
 sI = input()
-print("\nWould you like to use custom dimensions? Y/N\n")
-if input() == "Y" or input() == "y":
-    cd = True
-else:
-    cd = False
-if cd:
-    print("\nplease input the x dimension of the ASCII picture you want\n")
-    dimensionx = input()
-    print("\nplease input the y dimension of the ASCII picture you want\n")
-    dimensiony = input()
-else:
-    if sI.size[0]%2:
-        box = (0,0,sI.size[0]-1,sI.size[1])
-        sI = sI.crop(box)
-    if sI.size[1]%2:
-        box = (0,0,sI.size[0],sI.size[1]-1)
-        sI = sI.crop(box)
-    tempx = sI.size[0]/16
-    tempy = sI.size[1]/34
-#dimensions = int(dimensionx . dimensiony)
-print("\nOutput into a text file or onto this window? F or W\n")
-outputType = input()
 try:
     im = Image.open(sI)
 except IOError:
@@ -37,8 +15,46 @@ except:
     print("something went wrong with opening the file")
     input()
     exit
+print("\nWould you like to use custom dimensions? Y/N\n")
+if input() == "Y" or input() == "y":
+    cd = True
+    print("\nrecommended ratio is two x to one y\n")
+else:
+    cd = False
+if cd:
+    print("\nplease input the x dimension of the ASCII picture you want\n")
+    dimensionx = input()
+    print("\nplease input the y dimension of the ASCII picture you want\n")
+    dimensiony = input()
+else:
+    if im.size[0]%2:
+        box = (0,0,im.size[0]-1,im.size[1])
+        im = im.crop(box)
+    if im.size[1]%2:
+        box = (0,0,sI.size[0],sI.size[1]-1)
+        im = im.crop(box)
+    tempx = im.size[0] % 16
+    tempy = im.size[1] % 34
+    box = (0,0,im.size[0]-tempx,im.size[1]-tempy)
+    im = im.crop(box)
+    im.resize((im.size[0]/2,im.size[1]/2), resample=PIL.Image.LANCZOS)
+#dimensions = int(dimensionx + dimensiony)
+print("\nOutput into a text file or onto this window? F or W\n")
+outputType = input()
 gi = im.convert("L")
 #gi.save("testgray.jpeg")
+dimx = gi.size[0]/8
+dimy = gi.size[1]/17
+imTable = [[0 for x in range(dimx)] for y in range(dimy)]
+for x in range(dimx):
+    box = (8*x,0,8*x+7,gi.size[1])
+    imCol = gi.crop(box)
+    for y in range(dimy):
+        box = (0, 17*z, 7, 17*z+16)
+        imTable[x][y] = imCol.crop(box)
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,4 +1,36 @@
 import numpy as np
 
-array = np.load("asciArray.npy")
-print(array)
+npyArray = np.load("asciArray.npy")
+i = 0
+f = 0
+line = ""
+'''
+for array in npyArray:
+    for character in array:
+        if character == 255:
+            line += str(1)
+        elif character == 0:
+            line += str(0)
+        if i == 8:
+            line += "\n"
+            i = 0
+            f += 1
+        i += 1
+        if f == 17:
+            line+="\n\n"
+            f = 0
+    print(line)
+'''
+for x in range(32):
+    for y in range(17):
+        for z in range(8):
+            for char in npyArray[x*z][y*8:(y+1)*8]:
+                if char == 255:
+                    line += "1"
+                else:
+                    line += "0"
+            line += "  "
+        line += "\n"
+    line += "\n"
+    i += 1
+print(line)
